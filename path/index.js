d66842c2c463e416e15f7c7ac1be705a2dcc47b0
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-04-27 14:10:58 -0300

    Update index.js

--- path/index.js
+++ path/index.js
@@ -1,1 +1,28 @@
 //https://complex-humor-1df.notion.site/Trabalhando-com-Caminhos-no-Node-js-node-path-34ccc8c2db9680cabbebfc74b8cc75b4
+
+const path = require ('node:path');
+
+const dir = 'src';
+const file = 'app.js';
+
+const fullPath = path.join (dir, file);
+
+console.log (fullPath);
+
+const relativePath = '../arquivos/relatorio.pdf';
+
+const absolutePath = path.resolve (relativePath);
+
+console.log (absolutePath);
+
+const fileName = path.basename (relativePath);
+
+console.log (fileName);
+
+const folder = path.dirname (absolutePath);
+
+console.log (folder);
+
+const ext = path.extname (absolutePath);
+
+console.log (ext);
